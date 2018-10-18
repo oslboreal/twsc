3ec38398ffbc1961fac133e16b426e29bbb6ab7c
Vallejo Juan Marcos <oslboreal@gmail.com>
2018-10-18 00:44:54 -0300

    Add files via upload

---
+++ TWAnalyzer/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace TWAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer_Liker = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer_Liker
+            // 
+            this.timer_Liker.Enabled = true;
+            this.timer_Liker.Interval = 3000;
+            this.timer_Liker.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(512, 111);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(537, 141);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(553, 600);
+            this.MinimumSize = new System.Drawing.Size(55, 55);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TwAnalyzer -@Boreal_Arg - v2.1.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer_Liker;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TWAnalyzer/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TWAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer_Liker.Start();
+            Procesador.Proceso.Inicializar();
+            this.Log($"Juan Marcos Vallejo - {DateTime.Now} Inicializando.");
+        }
+
+        /// <summary>
+        /// Timer AUTO LIKER MENTIONS
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.richTextBox1.Text = "";
+            Log(Vista.InformacionProceso());
+        }
+
+        #region Métodos de Dibujado de User Interface
+
+        /// <summary>
+        /// Método de actualización de UI
+        /// </summary>
+
+        /// <summary>
+        /// Método encargado de mostrar por pantalla un anuncio
+        /// </summary>
+        /// <param name="text"></param>
+        private void Log(string text)
+        {
+            if (this.richTextBox1.Text == "")
+                this.richTextBox1.Text = this.richTextBox1.Text + DateTime.Now.ToString() + " - " + text;
+            else
+                this.richTextBox1.Text = this.richTextBox1.Text + "\n" + DateTime.Now.ToString() + " - " + text;
+        }
+
+        /// <summary>
+        /// Método encargado de mostrar por pantalla un anuncio con un reporte
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="report"></param>
+        private void Log(string text, string report)
+        {
+            if (this.richTextBox1.Text == "")
+                this.richTextBox1.Text = this.richTextBox1.Text + DateTime.Now.ToString() + " - " + text;
+            else
+                this.richTextBox1.Text = this.richTextBox1.Text + "\n" + DateTime.Now.ToString() + " - " + text;
+        }
+
+        /// <summary>
+        /// Método encargado de Registrar un comando en el Log.
+        /// </summary>
+        /// <param name="text"></param>
+        private void LogCommand(string text)
+        {
+            if (this.richTextBox1.Text == "")
+                this.richTextBox1.Text = this.richTextBox1.Text + DateTime.Now.ToString() + " - COMMAND:" + text;
+            else
+                this.richTextBox1.Text = this.richTextBox1.Text + "\n" + DateTime.Now.ToString() + " - COMMAND:" + text;
+        }
+        #endregion
+    }
+}
